362afa52263e8e4c5369432e7ee625a5179c4f42
Derek Guenther <dguenther9@gmail.com>
2021-04-10 22:29:21 -0700

    Add Github Actions test

--- Formula/ironfish.rb
+++ Formula/ironfish.rb
@@ -1,20 +1,17 @@
-require "formula"
-require_relative "../lib/download_strategy"
-
-URL = "https://ironfish-cli.s3.amazonaws.com/ironfish-cli-c4275d1.tar.gz"
-SHA = "6b2dc6fdb2c352523e7f3f1a4bbc6c20bf9b607790559c0789fc6faed2e7911b"
+URL = "https://ironfish-cli.s3.amazonaws.com/ironfish-cli-c4275d1.tar.gz".freeze
+SHA = "6b2dc6fdb2c352523e7f3f1a4bbc6c20bf9b607790559c0789fc6faed2e7911b".freeze
 
 class Ironfish < Formula
-  desc "Everything you need to get started with Ironfish"
+  desc "Everything you need to get started with Iron Fish"
   homepage "https://github.com/iron-fish/homebrew-brew"
+  url URL
+  sha256 SHA
   head "https://github.com/iron-fish/homebrew-brew.git"
   depends_on "node@14"
 
-  url URL
-  sha256 SHA
-
   def install
-    inreplace "bin/ironfish", /^CLIENT_HOME=/, "export IRONFISH_OCLIF_CLIENT_HOME=#{lib/"client"}/ironfish-cli\nCLIENT_HOME="
+    inreplace "bin/ironfish", /^CLIENT_HOME=/,
+"export IRONFISH_OCLIF_CLIENT_HOME=#{lib/"client"}/ironfish-cli\nCLIENT_HOME="
     inreplace "bin/ironfish", "\"$DIR/node\"", "#{Formula["node@14"].bin}/node"
 
     libexec.install Dir["*"]
