4275a6db5c2530f3d15a1a002314546a5a556d9c
NullSoldier <nullprogrammer@gmail.com>
2021-08-17 14:04:50 -0700

    Deploy brew from master

--- Formula/ironfishbeta.rb
+++ Formula/ironfishbeta.rb
@@ -1,6 +1,6 @@
-URL = "https://ironfish-cli.s3.amazonaws.com/ironfish-cli-0a8c7fd.tar.gz".freeze
-SHA = "83d27960940867574b24a56789b475577053da1adddd5bf32c79d5dccce92d70".freeze
-VERSION = "18".freeze
+URL = "https://ironfish-cli.s3.amazonaws.com/ironfish-cli-fb4f375.tar.gz".freeze
+SHA = "5a9ccbef61d036a9f22cf37f3ecdcaa6af5fd7a55be26820a0273ea8d719d35e".freeze
+VERSION = "19".freeze
 
 class Ironfishbeta < Formula
   desc "Beta distribution of Iron Fish"
